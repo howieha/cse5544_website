d19d1dab1c8368a24b5580010633190d90c849a4
Shijie Qu <howieha@163.com>
2020-04-22 23:15:23 -0400

    implement randomization

--- scripts/index.js
+++ scripts/index.js
@@ -45,7 +45,26 @@ function getGraphDataSets() {
 			.forceEngine('ngraph')
 	};
 
-	return [cluster15_node8, cluster30_node8, cluster15_node8_col, cluster30_node8_col]
+	return shuffle([cluster15_node8, cluster30_node8, cluster15_node8_col, cluster30_node8_col])
+}
+
+function shuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
 }
 
 const Graph = ForceGraphVR()
